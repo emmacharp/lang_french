ff4caf8299355279fcf30c92e85fb8c6512ccb6d
Pierre Bertet <bonjour@pierrebertet.net>
2010-02-09 18:59:42 +0100

    Initial import

---
+++ extension.driver.php
@@ -0,0 +1,34 @@
+<?php
+
+	Class extension_lang_french extends Extension {
+
+		/**
+		 * Extension information
+		 */
+
+		public function about() {
+			return array(
+				'name' => 'Language: French',
+				'type' => 'Localisation',
+				'version' => '1.0',
+				'release-date' => '2010-02-09',
+				'author' => array(
+					'name' => 'Grégoire Dierendonck / Pierre Bertet',
+					'website' => 'http://www.lefauteuil.fr',
+					'email' => 'contact@lefauteuil.fr'
+				),
+				'description' => 'French translation for the Symphony backend',
+				'compatibility' => array(
+					'2.0.0' => false,
+					'2.0.1' => false,
+					'2.0.2' => false,
+					'2.0.3' => false,
+					'2.0.4' => false,
+					'2.0.5' => false,
+					'2.0.6' => false,
+					'2.0.7' => true
+				)
+			);
+		}
+
+	}
